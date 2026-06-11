d192f6bf64df9117e27834cf60a4e271df507271
chancellor-whitaker <chancellor.whitaker@eku.edu>
2026-06-11 09:04:52 -0400

    revert

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 import { StrictMode } from "react";
 
 import "./index.css";
-import App from "./App3.jsx";
+import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
